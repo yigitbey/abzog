d102d15347ab0a0427e45d1ee451eaa573912b71
Yiğit Güneli <gro.tiiy@yiit.org>
2010-07-01 06:15:16 +0300

    adding different protocols

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ bus = dbus.SessionBus()
 obj = bus.get_object("im.pidgin.purple.PurpleService", "/im/pidgin/purple/PurpleObject") 
 purple = dbus.Interface(obj, "im.pidgin.purple.PurpleInterface") 
 
+protocols = {
+    "jabber": "prpl-jabber",
+    "facebook": "prpl-jabber",
+    "icq": "prpl-icq",
+    "msn": "prpl-msn",
+    "gtalk": "prpl-jabber",
+    "yahoo": "prpl-yahoo",
+} # List of protocols: http://developer.pidgin.im/wiki/prpl_id
+
 account_name = "grotiiy@jabber.org" # Must already be logged on.
-account_protocol = 'prpl-jabber' # List of protocols: http://developer.pidgin.im/wiki/prpl_id
+account_protocol = "jabber" # Change this with the platform
 message_to_send = "test" # Change this with your spam
 
-account = purple.PurpleAccountsFind(account_name, account_protocol) # Get the account information
+
+account = purple.PurpleAccountsFind(account_name, protocols[account_protocol]) # Get the account information
 
 buddylist = purple.PurpleFindBuddies(account,'') # Get a list of all buddies
 
 
-for buddy in buddylist: # Sending a message to all buddy list
+for buddy in buddylist: # Sending a message to all online buddies
     if purple.PurpleBuddyIsOnline(buddy): 
         message_to = purple.PurpleBuddyGetName(buddy) # Message receiver
         conversation = purple.PurpleConversationNew(1, account, message_to) # Create a new conversation with the receiver
